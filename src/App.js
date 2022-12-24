8307d98a6903362c90e82254f5b176f64ea6da34
Leshan <leshan0627@gmail.com>
2022-12-23 23:11:47 -0500

    Implement Bootstrap styles:

--- src/App.js
+++ src/App.js
@@ -2,37 +2,41 @@ import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   let [ticker, setTicker] = useState([]);
   let [search, setSearch] = useState("");
-  let [searchValue, setSearchValue] = useState("");
 
   axios
-    .get(`${searchValue.toUpperCase}`)
+    .get(`https://webapi.thecse.com/trading/listed/securities/${search}.json`)
     .then((res) => {
       setTicker(res.ticker);
+      console.log(res);
     })
     .catch((err) => {
       console.log(err);
     });
   return (
-    <div className='App'>
-      <form
-        onSubmit={(e) => {
-          setSearchValue(search);
-          console.log(search);
-        }}>
-        <input
-          type='text'
-          name='search'
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-          value={search}
-        />
-        <input type='submit' />
-      </form>
+    <div className='row App'>
+      <div className='wrap'>
+        <form>
+          <input
+            class='form-control'
+            id='exampleFormControlInput1'
+            type='text'
+            name='search'
+            placeholder='Enter A TSX stock symbol'
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            value={search}
+          />
+        </form>
+        <div></div>
+      </div>
     </div>
   );
 }
